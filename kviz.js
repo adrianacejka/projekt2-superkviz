3a86143952f1e7828a6d76f8e312e4bbc0bbce36
Adriana Čejka <adriana.cejka@seznam.cz>
2022-04-10 15:48:49 +0200

    otazky array and variables created

--- kviz.js
+++ kviz.js
@@ -1,8 +1,60 @@
 // Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
 
-const otazky = [];
+const otazky = [
+    {
+        otazka: 'Co je ikonická hračka z 80. let?',
+        obrazek: 'obrazky/moncicak.jpg',
+        moznosti: [
+            'kočičák', 'mončičák', 'opičák' 
+        ],
+        spravnaOdpoved: 'moznosti.indexOf("mončičák")'
+    },
+    {
+        otazka: 'Jaké je Adriany nejoblíbenější ovoce?',
+        obrazek: 'obrazky/ovoce.jpg',
+        moznosti: [
+            'kokos', 'jahoda', 'meloun', 'ani jedno'
+        ],
+        spravnaOdpoved: 'moznosti.indexOf("meloun")'
+    },
+    {
+        otazka: 'Pro absolvování kurzu je potřeba ...',
+        obrazek: 'obrazky/pivo.jpg',
+        moznosti: [
+            'Umět JavaScript', 'Chodit po kurzu do hospody'
+        ],
+        spravnaOdpoved: 'moznosti.indexOf("Umět JavaScript")'
+    }
+];
 
 // Dále budeš potřebovat další proměnné - jaké?
+
+// bílá plocha uprostřed stránky, obalující div
+let kviz = document.querySelector('.kviz');
+
+// pořadí otázky
+let poradi = document.querySelector('#poradi'); // <!--Sem můžeš zapsat pořadí otázky. opět pomocí JS-->
+
+// znění otázky
+let otazka = document.querySelector('#otazka'); // <!--A sem už přijde samotná otázka-->
+
+// obalujíci div, uvnitř obrázek + možnosti odpovědí
+let obsah = document.querySelector('.obsah');
+
+
+// vytvořím div pro obrázek k otázce
+let foto = document.createElement('div');
+foto.className = 'foto';
+
+// vytvořím element pro jednotlivé odpovědi
+let moznosti = document.createElement('div');
+moznosti.id = 'moznosti';
+
+// <!--A nakonci bude tady výsledek - nejdřív vidět není a poté ano. U otázek je to opačně.-->
+let vysledek = document.querySelector('.vysledek');
+let hodnoceni = document.querySelector('#hodnoceni');
+
+
 
 
 // Tato funkce se postará o vygenerování otázky
